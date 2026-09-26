391a861ceb9f8dfbb0d4acfc5b1b900bffa10de5
EnsinoFisica <ensino.fisica.astronomia@gmail.com>
2026-09-26 16:31:34 -0300

    Configuracoes adicionais deploy via github

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "https://github.com/EnsinoFisicaAstronomia/Jogo-Da-Lua", 
   plugins: [react(), tailwindcss()],
 });
